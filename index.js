3d45eeee40ea182b7189420439bb5dcbd3b09c3c
fahim-khandakar <fahimkhandakar01@gmail.com>
2023-11-03 11:45:40 +0600

    added all file

---
+++ index.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.p0m1q4c.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    const bookCollections = client.db("bookDB").collection("book");
+
+    app.post("/books", async (req, res) => {
+      const newBook = req.body;
+      const result = await bookCollections.insertOne(newBook);
+      res.send(result);
+    });
+
+    app.get("/books", async (req, res) => {
+      const cursor = bookCollections.find();
+      const results = await cursor.toArray();
+      res.send(results);
+    });
+
+    app.delete("/books/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollections.deleteOne(query);
+      res.send(result);
+    });
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("todo list server is running");
+});
+
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
